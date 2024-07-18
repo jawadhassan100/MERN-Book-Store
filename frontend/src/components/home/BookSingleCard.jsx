159222583622c795227ffdecb10e066f51305bd8
Jawad Hassan <jawadhassankhan2001@gmail.com>
2024-07-19 00:01:07 +0500

    card updated

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -11,7 +11,7 @@ const BookSingleCard = ({ book }) => {
 
   return (
     <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
-      <h2 className='absolute top-1 right-1 px-5 py-2 bg-red-300 rounded-lg'>
+      <h2 className='absolute top-1 right-4 px-4 py-1 bg-red-300 rounded-lg'>
         {book.publishYear}
       </h2>
       <h4 className='my-2 text-gray-500'>{book._id}</h4>
